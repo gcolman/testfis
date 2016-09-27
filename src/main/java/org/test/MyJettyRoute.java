84181a3547719fdd67b513baa928ec21f806035b
gcolman <gcolman@localhost.localdomain>
2016-09-27 18:58:52 +0100

    i

--- src/main/java/org/test/MyJettyRoute.java
+++ src/main/java/org/test/MyJettyRoute.java
@@ -42,7 +42,7 @@ public class MyJettyRoute extends RouteBuilder {
 		    .log("${body}")
                 .otherwise()
 		    .log("${body}")	
-                    .transform(simple(""));
+                    .transform(simple(" {'pod':'${sysenv.HOSTNAME}' , 'colour' : 'green', 'checked':0 }"));
    	  		 
    }
 
